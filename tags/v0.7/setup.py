d21f149149636604dd2c7ae24620b74d161bfa6b
marcel.martin@tu-dortmund.de <marcel.martin@tu-dortmund.de@1026ca1b-a64f-2ca6-1c27-9310f0dfb46e>
2010-12-03 12:45:00 +0000

    tag v0.7

---
+++ tags/v0.7/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup, Extension
+import sys
+
+if sys.version_info < (2, 6):
+	print "At least Python 2.6 is required."
+	sys.exit(1)
+
+module = Extension('calign', sources = [ 'calignmodule.c' ])
+
+setup(name = 'cutadapt',
+	version = '0.7',
+	description = 'trim adapters from high-throughput sequencing reads',
+	author = 'Marcel Martin',
+	author_email = 'marcel.martin@tu-dortmund.de',
+	url = 'http://cutadapt.googlecode.com/',
+	license = 'MIT',
+	ext_modules = [ module ],
+	py_modules = ['fasta', 'align'],
+	scripts = ['cutadapt']
+)
